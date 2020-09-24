ff2745cc9ad1134741cab16bca0c48672daa7717
Phill Wiggins <phill.wiggins@gmail.com>
2020-09-24 14:25:49 +0100

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,75 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    id("kotlin-android-extensions")
+}
+
+android {
+    compileSdkVersion(Sdk.COMPILE_SDK_VERSION)
+
+    defaultConfig {
+        minSdkVersion(Sdk.MIN_SDK_VERSION)
+        targetSdkVersion(Sdk.TARGET_SDK_VERSION)
+
+        applicationId = App.APP_ID
+        versionCode = App.APP_VERSION_CODE
+        versionName = App.APP_VERSION_NAME
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    sourceSets {
+        getByName("main").java.srcDirs("src/main/kotlin")
+    }
+    buildTypes {
+        getByName("debug") {
+            isMinifyEnabled = false
+            isDebuggable = true
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-dev"
+        }
+        getByName("release") {
+            isMinifyEnabled = true
+            isDebuggable = false
+            isShrinkResources = true
+            isZipAlignEnabled = true
+            isJniDebuggable = false
+            isRenderscriptDebuggable = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    lintOptions {
+        isWarningsAsErrors = true
+        isAbortOnError = true
+    }
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk7"))
+
+    implementation(SupportLibs.ANDROIDX_APPCOMPAT)
+    implementation(SupportLibs.ANDROIDX_CONSTRAINT_LAYOUT)
+    implementation(SupportLibs.ANDROIDX_CORE_KTX)
+    implementation(SupportLibs.MATERIAL)
+    implementation(SupportLibs.ANDROIDX_NAV_FRAGMENT)
+    implementation(SupportLibs.ANDROIDX_NAV_UI)
+
+    testImplementation(TestingLib.JUNIT)
+
+    androidTestImplementation(AndroidTestingLib.ANDROIDX_TEST_EXT_JUNIT)
+    androidTestImplementation(AndroidTestingLib.ANDROIDX_TEST_RULES)
+    androidTestImplementation(AndroidTestingLib.ESPRESSO_CORE)
+}
+
+tasks.register("installGitHook", Copy::class.java) {
+    from("${rootProject.rootDir}/config/commit/pre-commit")
+    into("${rootProject.rootDir}/.git/hooks")
+}
+
+tasks.getByPath(":app:preBuild").dependsOn("installGitHook")
